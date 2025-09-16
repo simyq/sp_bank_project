3535243650ae09739374f1e3487ff6f615f7e349
Maksim S <Mail>
2025-09-16 15:43:07 +0300

    small fixes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-"""Main module of the program, currently contains only tests for masks.py"""
+"""Main module of the program, currently contains only tests for all the modules"""
 
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
 
 def main() -> None:
@@ -14,18 +15,39 @@ def main() -> None:
         None
     """
 
-    print(get_mask_card_number(1234567890123456))
-    print(get_mask_card_number(123456789012))
-    print(get_mask_card_number(None))
-    print(get_mask_card_number(True))
-    print(get_mask_card_number(123456789.12))
+    print(get_mask_card_number("1234567890123456"))
+    print(get_mask_card_number("123456789012"))
+    print(get_mask_card_number("123456789.12"))
 
-    print(get_mask_account(73654108430135874305))
-    print(get_mask_account(1234567890123456))
-    print(get_mask_account(123456789012))
-    print(get_mask_account(None))
-    print(get_mask_account(True))
-    print(get_mask_account(123456789))
+    print(get_mask_account("73654108430135874305"))
+    print(get_mask_account("1234567890123456"))
+    print(get_mask_account("123456789012"))
+    print(get_mask_account("123456789"))
+
+    print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(mask_account_card("Maestro 1596837868705199"))
+    print(mask_account_card("MasterCard 7158300734726758"))
+    print(mask_account_card("Visa Classic 6831982476737658"))
+    print(mask_account_card("Visa Platinum 8990922113665229"))
+    print(mask_account_card("Visa Gold 5999414228426353"))
+
+    print(mask_account_card("Visa Gold 5999414228426.53"))
+
+    print(mask_account_card("Счет 64686473678894779589"))
+    print(mask_account_card("Счет 73654108430135874305"))
+    print(mask_account_card("Счет 7365410843013587123"))
+    print(mask_account_card("Счет 35383033474447895560"))
+
+    print(mask_account_card("Счёт 35383033474447895560"))
+    print(mask_account_card("Счет 35383033474447895.60"))
+
+    print(mask_account_card(None))
+    print(mask_account_card(False))
+    print(mask_account_card(1546645456654))
+
+    print(get_date("2024-03-11T02:26:18.671407"))
+    print(get_date("20203-11T02:26:18.671407"))
+    print(get_date("dsdfghjk"))
 
 
 if __name__ == "__main__":
--- src/widget.py
+++ src/widget.py
@@ -2,13 +2,13 @@
 Module for hiding personal bank data (account number and card number)
 Additionally has function for making date formatting
 """
+
 from datetime import datetime
 from typing import Any
 
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
-credit_card_names = ['visa', 'maestro', 'mastercard', 'белкарт', 'мир']
-
+credit_card_names = ["visa", "maestro", "mastercard", "белкарт", "мир"]
 
 
 def mask_account_card(bank_data: Any) -> str:
@@ -20,7 +20,8 @@ def mask_account_card(bank_data: Any) -> str:
         in format 'Счет 73654108430135874305' or 'Visa Platinum 7000792289606361'
 
     Returns:
-        str: masked personal bank data (account number and bank card number) or 'Invalid input' if validation fails
+        str: masked personal bank data (account number and bank card number)
+        or 'Invalid input' if validation fails (not string type given)
 
     Examples:
         Visa Platinum 7000792289606361 -> Visa Platinum 7000 79** **** 6361
@@ -29,24 +30,31 @@ def mask_account_card(bank_data: Any) -> str:
 
     try:
         modified_bank_data = bank_data.strip().lower().split()
-        if modified_bank_data[0] in credit_card_names and len(modified_bank_data[-1]) == 16 and modified_bank_data[-1].isdigit():
-            card_number = int(modified_bank_data[-1]) # Has to be fixed (no int())
+        if (
+            modified_bank_data[0] in credit_card_names
+            and len(modified_bank_data[-1]) == 16
+            and modified_bank_data[-1].isdigit()
+        ):
+            card_number = modified_bank_data[-1]
             masked_card_number = get_mask_card_number(card_number)
             hidden_bank_data = f"{' '.join(modified_bank_data[:-1]).title()} {masked_card_number}"
 
-        elif modified_bank_data[0] == 'счет' or 'счёт' and modified_bank_data[-1] == 20 and modified_bank_data[-1].isdigit():
-            bank_account_number = int(modified_bank_data[-1])  # Has to be fixed (no int())
+        elif (
+            modified_bank_data[0] in ("счет", "счёт")
+            and len(modified_bank_data[-1]) == 20
+            and modified_bank_data[-1].isdigit()
+        ):
+            bank_account_number = modified_bank_data[-1]
             masked_account_number = get_mask_account(bank_account_number)
             hidden_bank_data = f"{' '.join(modified_bank_data[:-1]).title()} {masked_account_number}"
 
         else:
-            return 'Invalid input'
+            return "Invalid data given"
 
         return hidden_bank_data
 
-    except TypeError:
-
-        return 'Invalid input'
+    except Exception:
+        return "Invalid input"
 
 
 def get_date(date: str) -> str:
@@ -58,8 +66,12 @@ def get_date(date: str) -> str:
 
     Returns:
         formatted_date (str) in format DD.MM.YYYY
+        or 'Invalid date' if validation fails (not ISO-format or string type given)
     """
 
-    formatted_date = datetime.fromisoformat(date).strftime("%d.%m.%Y")
+    try:
+        formatted_date = datetime.fromisoformat(date).strftime("%d.%m.%Y")
+        return formatted_date
 
-    return formatted_date
+    except ValueError:
+        return "Invalid date"
